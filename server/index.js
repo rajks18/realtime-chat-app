7e1b987c2e984d337d212f31ca5732e821114936
Rajkumar Sarker <sardonic100@gmail.com>
2026-03-27 18:26:42 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const io = initializeSocket(server);
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://real-time-chat288.vercel.app'  // your exact Vercel URL
+    'https://rajgram28.vercel.app/'  // your exact Vercel URL
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
